770d3709965bf03d5dfe4b9ccf1989218146f79a
sc-contenthub-ci-github <sc-contenthub-ci-github@sitecore.com>
2022-10-06 13:33:18 +0000

    Bump version to 1.1.4

--- Formula/ch-cli.rb
+++ Formula/ch-cli.rb
@@ -1,12 +1,12 @@
 class ChCli < Formula
-  version "1.1.2"
+  version "1.1.4"
 
   if OS.mac?
-    url "https://github.com/Sitecore/content-hub-cli/releases/download/1.1.2/ch-cli-osx-x64.1.1.2.zip"
-    sha256 "9d7904f505043a0afb7d8063dc982d769ce1c64ecc01155b88c96640cf31dcb7"
+    url "https://github.com/Sitecore/content-hub-cli/releases/download/1.1.4/ch-cli-osx-x64.1.1.4.zip"
+    sha256 "e7629815dbf5570825e9eddd4b6c2126997fc53621f19607d623ef27d297ad22"
   elsif OS.linux?
-    url "https://github.com/Sitecore/content-hub-cli/releases/download/1.1.2/ch-cli-linux-x64.1.1.2.tar.gz"
-    sha256 "922e7bc9585100a7865d29f0774740317e7679017cb89bf9a702955af5b6e01d"
+    url "https://github.com/Sitecore/content-hub-cli/releases/download/1.1.4/ch-cli-linux-x64.1.1.4.tar.gz"
+    sha256 "834c57484935359e57b39d4ece3d6e374ba5fc2fd583656c4db3e41e49b592b4"
   end
 
   desc "Command-line interface for Sitecore Content Hub"
