373870850ba56451580e2818369d4a4e01382350
sc-contenthub-ci-github <sc-contenthub-ci-github@sitecore.com>
2023-03-20 13:51:06 +0000

    Bump ch-one-cli to 1.1.1

--- Formula/ch-one-cli.rb
+++ Formula/ch-one-cli.rb
@@ -1,12 +1,12 @@
 class ChOneCli < Formula
-  version "1.1.0"
+  version "1.1.1"
 
   if OS.mac?
-    url "https://github.com/Sitecore/content-hub-one-cli/releases/download/1.1.0/ch-one-cli-osx-x64.1.1.0.zip"
-    sha256 "43615aba88f15827c2f0b83e766818f00de18e5b17d361e2acc050726a265b02"
+    url "https://github.com/Sitecore/content-hub-one-cli/releases/download/1.1.1/ch-one-cli-osx-x64.1.1.1.zip"
+    sha256 "4e85927642c0041878d9cd77292fe96dc053422183b97d51d5e09ac20273f43c"
   elsif OS.linux?
-    url "https://github.com/Sitecore/content-hub-one-cli/releases/download/1.1.0/ch-one-cli-linux-x64.1.1.0.tar.gz"
-    sha256 "f250699ae416372d767a5dba441e55a85f71c3f7d905a451a6eae74c19d3b6c8"
+    url "https://github.com/Sitecore/content-hub-one-cli/releases/download/1.1.1/ch-one-cli-linux-x64.1.1.1.tar.gz"
+    sha256 "59568fa1e07a1356c3a7e096e08c46e39a461d97d82188d7de559e0a7f1cba7d"
   end
 
   desc "A command-line interface (CLI) for interacting with Content Hub ONE"
