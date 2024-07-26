ae8236f55f0761af40ed13953343da56b1e3fde0
Charalampos Vitoros (Χαράλαμπος Βιτώρος) <cvi@sitecore.net>
2024-07-26 16:15:42 +0300

    Update ch-one-cli.rb

--- Formula/ch-one-cli.rb
+++ Formula/ch-one-cli.rb
@@ -3,10 +3,10 @@ class ChOneCli < Formula
 
   if OS.mac?
     url "https://github.com/Sitecore/content-hub-one-cli/releases/download/2.0.1/ch-one-cli-osx-x64.2.0.1.zip"
-    sha256 "10ac6791d4990de5869709fd116c468b7ebde78eeeeb4fea88704d832389d6a0"
+    sha256 "f1ce9d810335cd9cce5c0da2080bfaab764f0fdfddb6467f3c1dcd5007ba0d27"
   elsif OS.linux?
     url "https://github.com/Sitecore/content-hub-one-cli/releases/download/2.0.1/ch-one-cli-linux-x64.2.0.1.tar.gz"
-    sha256 "685e4d443076caa6b440d98663082e6295357de7c5032dedaa8ede4c2c2de0d4"
+    sha256 "3232205cfec788e625de2241b17e2515a8831fdc3522eb2fc966ecbdc0a6087a"
   end
 
   desc "A command-line interface (CLI) for interacting with Content Hub ONE"
